7fa1b4cad3bced86dffcc5b1e28dab078beaa7d0
Roland Graf <oxigen.rg@gmail.com>
2017-10-29 04:23:49 +0100

    Add files via upload

--- e107_plugins/download/languages/Czech/Czech_log.php
+++ e107_plugins/download/languages/Czech/Czech_log.php
@@ -4,14 +4,17 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:15:25
+|        Last Modified: 2017/10/28 12:07:56
 |
-|        $Author: Oxigen $
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/28 12:07:56 $
+|        $Author: Oxigen O2 $
 +---------------------------------------------------------------+
 */
 
-define("LAN_AL_DOWNL_01", "Ke stažení - možnosti stahování změnil");
-define("LAN_AL_DOWNL_02", "Ke stažení - možnosti odeslání změněn");
+define("LAN_AL_DOWNL_01", "Ke stažení - možnosti stahování byly změněny");
+define("LAN_AL_DOWNL_02", "Ke stažení - možnosti nahrávání byly změněny");
 define("LAN_AL_DOWNL_03", "Ke stažení - přidáno omezení");
 define("LAN_AL_DOWNL_04", "Ke stažení - log 04");
 define("LAN_AL_DOWNL_05", "Ke stažení - log 05");
@@ -19,7 +22,8 @@ define("LAN_AL_DOWNL_06", "Ke stažení - log 06");
 define("LAN_AL_DOWNL_07", "Ke stažení - log 07");
 define("LAN_AL_DOWNL_08", "Ke stažení - log 08");
 define("LAN_AL_DOWNL_09", "Ke stažení - log 09");
-define("LAN_AL_DOWNL_10", "Ke stažení - limit aktualizován");
-define("LAN_AL_DOWNL_11", "Ke stažení - limit odstraněny");
+define("LAN_AL_DOWNL_10", "Ke stažení - limity aktualizovány");
+define("LAN_AL_DOWNL_11", "Ke stažení - limity odstraněny");
 
 
+?>
