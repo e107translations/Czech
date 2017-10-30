ee85b807027eb5fa063424d8090e32b78eeb3db4
Roland Graf <oxigen.rg@gmail.com>
2017-10-30 22:24:29 +0100

    Translate plugin FAQ

--- e107_plugins/faqs/languages/Czech/Czech_front.php
+++ e107_plugins/faqs/languages/Czech/Czech_front.php
@@ -1,19 +1,28 @@
 <?php
 /*
-+---------------------------------------------------------------+
-|        e107 website content management system Czech Language File
-|        Released under the terms and conditions of the
-|        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:15:35
-|
-|        $Author: Oxigen $
-+---------------------------------------------------------------+
+ * e107 website system
+ *
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * FAQ Plugin Global Language Definitions
+ *
 */
 
-define("LAN_FAQS_TAGS", "Značky");
-define("LAN_FAQS_FILTER_ACTIVE", "Filtr aktivní");
-define("LAN_FAQS_NONE_AVAILABLE", "V současné době nejsou k dispozici žádné dotazy.");
-define("LAN_FAQS_ASKQUESTION_AFTER", "Děkuju. Vaše otázka byla uložena a budou zodpovězeny co nejdříve.");
-
-
-?>
+define("LAN_FAQS_001", "Tagy");
+define("LAN_FAQS_002", "Filtr");
+define("LAN_FAQS_003", "V současné době nejsou k dispozici žádné dotazy.");
+define("LAN_FAQS_004", "Děkuji, Vaše otázka byla uložena a doufejme, že bude brzy zodpovězena.");
+define("LAN_FAQS_005", "Dosáhli jste maximálního počtu nových otázek. Můžete položit další otázku, jakmile budou vaše stávající otázky zodpovězeny.");
+define("LAN_FAQS_006", "Odstranit");
+define("LAN_FAQS_007", "Resetovat");
+define("LAN_FAQS_008", "[x]: Nebylo nalezeno.");
+define("LAN_FAQS_009", "moderovat komentáře");
+define("LAN_FAQS_010", "Zpět na kategorie");
+define("LAN_FAQS_011", "FAQ");
+define("LAN_FAQS_012", "Zadejte svou otázku zde...");
+define("LAN_FAQS_013", "maximální počet znaků");
+define("LAN_FAQS_014", "Pro vložení otazky se prosím [register] nebo se přihlaste.");
+define("LAN_FAQS_015", "Nepovoleno.");
+define("LAN_FAQS_016", "Byli jste požádáni o odpověď na následující otázky.");
--- e107_plugins/faqs/languages/Czech/Czech_global.php
+++ e107_plugins/faqs/languages/Czech/Czech_global.php
@@ -1,11 +1,16 @@
 <?php
+/*
+* Copyright (c) e107 Inc 2008-2013 - e107.org, 
+* Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+*
+* 'FAQ plugin' global language definitions
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+define("LAN_PLUGIN_FAQS_NAME", 			"FAQ");
+define("LAN_PLUGIN_FAQS_DESCRIPTION", 	"Jednoduchý doplněk pro přidání (FAQ) Často kladené otázky.");
+define("LAN_PLUGIN_FAQS_FUNCTIONNAME",	"FAQ kategorie");
+define("LAN_PLUGIN_FAQS_FRONT_NAME", 	"FAQ (Často kladené otázky)");
+define("LAN_PLUGIN_FAQS_SEARCH",		"FAQ vyhledávání");
+define("LAN_FAQS_ASK_A_QUESTION",		"Položit otázku");
 
-define("LAN_PLUGIN_FAQS_NAME", "Nejčastější dotazy");
-define("LAN_PLUGIN_FAQS_DESCRIPTION", "Jednoduchý plugin přidat nejčastější dotazy k webu.");
-define("LAN_PLUGIN_FAQS_FUNCTIONNAME", "Kategorie FAQ");
-define("LAN_PLUGIN_FAQS_FRONT_NAME", "FAQ (často kladené otázky)");
-define("LAN_PLUGIN_FAQS_SEARCH", "FAQ Hledat");
+
