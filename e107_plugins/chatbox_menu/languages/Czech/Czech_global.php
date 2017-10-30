032d08480ba3e6dcb112287b4397201eeb972238
Roland Graf <oxigen.rg@gmail.com>
2017-10-30 13:48:10 +0100

    Translation, spelling check Chatbox plugin

--- e107_plugins/chatbox_menu/languages/Czech/Czech_global.php
+++ e107_plugins/chatbox_menu/languages/Czech/Czech_global.php
@@ -4,23 +4,28 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:10:02
+|        Last Modified: 2017/10/30 13:21:27
 |
-|        $Author: Oxigen $
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/30 13:21:27 $
+|        $Author: Oxigen O2 $
 +---------------------------------------------------------------+
 */
+
 define("LAN_PLUGIN_CHATBOX_MENU_NAME", "Chatbox");
-define("LAN_PLUGIN_CHATBOX_MENU_DESCRIPTION", "Chatbox Menu");
-define("LAN_PLUGIN_CHATBOX_MENU_POSTS", "příspěvky chatboxu");
+define("LAN_PLUGIN_CHATBOX_MENU_DESCRIPTION", "Chatbox menu");
+define("LAN_PLUGIN_CHATBOX_MENU_POSTS", "Příspěvky v Chatboxu");
 define("LAN_AL_CHBLAN_01", "Chatbox nastavení aktualizováno");
 define("LAN_AL_CHBLAN_02", "Chatbox prořezávají");
 define("LAN_AL_CHBLAN_03", "Chatbox příspěvky přepočítány");
 define("LAN_AL_CHBLAN_04", "");
 define("LAN_AL_CHBLAN_05", "");
-define("NT_LAN_CB_1", "Události chatboxu");
-define("NT_LAN_CB_2", "Odesláno");
-define("NT_LAN_CB_3", "Napsal");
+define("NT_LAN_CB_1", "Chatbox události");
+define("NT_LAN_CB_2", "Zpráva odeslána");
+define("NT_LAN_CB_3", "Přidal");
 define("NT_LAN_CB_5", "Zpráva");
-define("NT_LAN_CB_6", "Zpráva odeslána");
+define("NT_LAN_CB_6", "Chat zpráva odeslána");
 
 
+?>
--- e107_plugins/chatbox_menu/languages/Czech/admin_chatbox_menu.php
+++ e107_plugins/chatbox_menu/languages/Czech/admin_chatbox_menu.php
@@ -4,9 +4,12 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:10:20
+|        Last Modified: 2017/10/30 13:32:22
 |
-|        $Author: Oxigen $
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/30 13:32:22 $
+|        $Author: Oxigen O2 $
 +---------------------------------------------------------------+
 */
 
@@ -19,7 +22,7 @@ define("CHBLAN_24", "Jeden den");
 define("CHBLAN_25", "Jeden týden");
 define("CHBLAN_26", "Jeden měsíc");
 define("CHBLAN_27", "- Smazat všechny příspěvky -");
-define("CHBLAN_29", "Zobrazit chatbox uvnitř posouvání vrstvy s výškou [x]");
+define("CHBLAN_29", "Zobrazit chatbox v posouvacím oknu");
 define("CHBLAN_31", "Zobrazovat smajlíky");
 define("CHBLAN_32", "Třída moderátorů");
 define("CHBLAN_33", "Počty uživatelů aktualizovány");
@@ -28,6 +31,7 @@ define("CHBLAN_35", "Aktualizovat");
 define("CHBLAN_36", "Možnosti zobrazení chatboxu");
 define("CHBLAN_37", "Běžný chatbox");
 define("CHBLAN_38", "Aktualizovat příspěvky dynamicky pomocí javascriptu (AJAX)");
-define("CHBLAN_42", "Zobrazit částku příspěvků v profilu uživatele");
+define("CHBLAN_42", "Zobrazit počet příspěvků v profilu uživatele");
 
 
+?>
