f5a3c23f37e44a54941b79a1b3b3a74f0a535291
Roland Graf <oxigen.rg@gmail.com>
2017-10-30 14:05:49 +0100

    Translation and checking banner menu plugin

--- e107_plugins/clock_menu/languages/admin/Czech.php
+++ e107_plugins/clock_menu/languages/admin/Czech.php
@@ -4,21 +4,26 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:10:31
+|        Last Modified: 2017/10/30 14:02:03
 |
-|        $Author: Oxigen $
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/30 14:02:03 $
+|        $Author: Oxigen O2 $
 +---------------------------------------------------------------+
 */
-define("CLOCK_AD_L2", "Záhlaví");
-define("CLOCK_AD_L4", "Nastavení menu hodin");
+
+define("CLOCK_AD_L2", "Titulek");
+define("CLOCK_AD_L4", "Nastavení menu s hodinami");
 define("CLOCK_AD_L5", "AM/PM");
-define("CLOCK_AD_L6", "Je-li políčko zaškrtnuto, zobrazí se čas s USA formát (0-12 hod/formát). Zaškrtnuta, že zobrazí se formát 'vojenské' formát 0-24");
-define("CLOCK_AD_L7", "Předpona data");
-define("CLOCK_AD_L8", "Pokud váš jazyk vyžaduje krátké slovo před datem (např. ' le' za francouzské) nebo 'den' pro němčinu..., použijte toto pole. Pokud není požadováno, ponechejte prázdné.");
+define("CLOCK_AD_L6", "Zaškrtněte pro US formát hodin (0-12 AM/PM). Nechte nezaškrtnuto pro standardní 24 hodinový formát");
+define("CLOCK_AD_L7", "Předpony datumu");
+define("CLOCK_AD_L8", "Textová zkratka před datem, pokud to váš jazyk vyžaduje (např. 'le' ve Francouzštině nebo 'den' v Němčině...). Pokud není potřeba, nechte prázdné.");
 define("CLOCK_AD_L9", "Přípona pro 1");
 define("CLOCK_AD_L10", "Přípona pro 2");
 define("CLOCK_AD_L11", "Přípona pro 3");
 define("CLOCK_AD_L12", "Přípona pro 4 a další");
-define("CLOCK_AD_L13", "Vyžaduje-li váš jazyk zobrazení příponu těsně po čísel pro datum, vyplňte tato pole s příponou pouze (příklad: 'st' 1, 'nd' 2, 'rd' pro 3 a 'th' pro 4 a více - anglicky). Není-li požadovaný ponechejte prázdné.");
+define("CLOCK_AD_L13", "Textová přípona za datem, pokud to váš jazyk vyžaduje (např.: 'st' pro 1, 'nd' pro 2, 'rd' pro 3 a 'th' pro 4 a další - pro angličtinu). Pokud není potřeba, nechte prázdné.");
 
 
+?>
