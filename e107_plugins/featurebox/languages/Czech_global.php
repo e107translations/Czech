51ceb4569ca740be52fe1aa29c1e8ded10671c41
Roland Graf <oxigen.rg@gmail.com>
2017-10-30 14:56:45 +0100

    Add files via upload

--- e107_plugins/featurebox/languages/Czech_global.php
+++ e107_plugins/featurebox/languages/Czech_global.php
@@ -4,19 +4,23 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:16:09
+|        Last Modified: 2017/10/30 14:55:35
 |
-|        $Author: Oxigen $
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/30 14:55:35 $
+|        $Author: Oxigen O2 $
 +---------------------------------------------------------------+
 */
 
-define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
-define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Displays an animated area on the top of your page with news-items and other content you would like to feature.");
-define("LAN_PLUGIN_FEATUREBOX_BATCH", "Vytvořit položku Featurebox");
-define("FBLAN_INSTALL_01", "Adding Default category table data.");
-define("FBLAN_INSTALL_02", "Adding Default table data.");
+define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature box");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Tento doplněk vám umožňuje zobrazit blok s textem nad vašimi novinkami. Texty se mohou také měnit, buď náhodně nebo podle daného pořadí.");
+define("LAN_PLUGIN_FEATUREBOX_BATCH", "Přidat položku");
+define("FBLAN_INSTALL_01", "Přidání výchozích kategorií.");
+define("FBLAN_INSTALL_02", "Přidání výchozích hodnot.");
 define("FBLAN_INSTALL_03", "Nepřiřazené");
 define("FBLAN_INSTALL_04", "Carousel");
 define("FBLAN_INSTALL_05", "Tabulátory");
 
 
+?>
