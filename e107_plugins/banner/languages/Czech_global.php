838700d4b0bb282f28f9f2e567e120d639a9e44d
Roland Graf <oxigen.rg@gmail.com>
2017-10-30 13:44:21 +0100

    Translation and checking banner menu plugin

--- e107_plugins/banner/languages/Czech_global.php
+++ e107_plugins/banner/languages/Czech_global.php
@@ -4,13 +4,17 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:09:39
+|        Last Modified: 2017/10/30 12:57:20
 |
-|        $Author: Oxigen $
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/30 12:57:20 $
+|        $Author: Oxigen O2 $
 +---------------------------------------------------------------+
 */
 
 define("LAN_PLUGIN_BANNER_NAME", "Bannery");
-define("LAN_PLUGIN_BANNER_DESCRIPTION", "Přidat k webu e107 reklamních bannerů");
+define("LAN_PLUGIN_BANNER_DESCRIPTION", "Přidá reklamní bannery na vaše webové stránky.");
 
 
+?>
