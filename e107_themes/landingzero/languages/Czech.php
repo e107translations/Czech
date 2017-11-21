9367f1a68906e79abdea882eeb57d6d2d4e9b513
Roland Graf <oxigen.rg@gmail.com>
2017-11-21 22:53:33 +0100

    Updated to 2.1.7-alpha

--- e107_themes/landingzero/languages/Czech.php
+++ e107_themes/landingzero/languages/Czech.php
@@ -1,25 +1,31 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/11/07 00:38:49
+|
+|        $Author: OxigenO2 $
++---------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_LZ_THEME_00", "Ok");
+define("LAN_LZ_THEME_00", "OK");
 define("LAN_LZ_THEME_01", "Začínáme");
-define("LAN_LZ_THEME_02", "Přepnout Video");
-define("LAN_LZ_THEME_03", "Váš prohlížeč nepodporuje video tag. Navrhuji, že aktualizujte svůj prohlížeč.");
-define("LAN_LZ_THEME_04", "Flexibilní rozložení");
+define("LAN_LZ_THEME_02", "Toggle Video");
+define("LAN_LZ_THEME_03", "Váš prohlížeč nepodporuje video tag. Doporučujeme, abyste aktualizovat svůj prohlížeč.");
+define("LAN_LZ_THEME_04", "Flexibilní rozvržení");
 define("LAN_LZ_THEME_05", "Zavřít");
-define("LAN_LZ_THEME_06", "Funkce");
+define("LAN_LZ_THEME_06", "Vlastnosti");
 define("LAN_LZ_THEME_07", "Zadejte prosím zprávu.");
 define("LAN_LZ_THEME_08", "Telefonní číslo");
-define("LAN_LZ_THEME_09", "Prosím zadejte své telefonní číslo.");
+define("LAN_LZ_THEME_09", "Prosím, zadejte své telefonní číslo.");
 define("LAN_LZ_THEME_10", "Následovat");
 define("LAN_LZ_THEME_11", "Telefon");
-define("LAN_LZ_THEME_12", "Vaše zpráva...");
-define("LAN_LZ_THEME_13", "Buďte v kontaktu");
-define("LAN_LZ_THEME_14", "Milujeme zpětnou vazbu. Vyplňte formulář níže a cigarety se zpět k vám co nejdříve.");
-define("LAN_LZ_THEME_15", "Sdělte nám svůj e-mail");
-define("LAN_LZ_THEME_16", "Přihlaste se k odběru aktualizací");
-define("LAN_LZ_THEME_17", "Zůstat zaúčtované");
-define("LAN_LZ_THEME_18", "MŮŽETE TAKÉ LÍBÍ");
+define("LAN_LZ_THEME_12", "Vaše zpráva zde ...");
+define("LAN_LZ_THEME_13", "Být v kontaktu");
+define("LAN_LZ_THEME_14", "Milujeme zpětnou vazbu. Vyplňte níže uvedený formulář a my dostat zpět k vám co nejdříve.");
+define("LAN_LZ_THEME_15", "Řekněte nám svůj e-mail");
+define("LAN_LZ_THEME_16", "Odebírat novinky");
+define("LAN_LZ_THEME_17", "Publikováno");
+define("LAN_LZ_THEME_18", "MOHLO BY SE VÁM TAKÉ LÍBIT");
--- e107_themes/landingzero/languages/Czech_admin.php
+++ e107_themes/landingzero/languages/Czech_admin.php
@@ -1,13 +1,19 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/11/07 00:41:31
+|
+|        $Author: OxigenO2 $
++---------------------------------------------------------------+
+*/
 
 define("LAN_LZ_THEMEPREF_00", "Obrázek na pozadí pro video [1920x1080px]");
 define("LAN_LZ_THEMEPREF_01", "Obrázek na pozadí pro mobilní zařízení");
 define("LAN_LZ_THEMEPREF_02", "První snímek videa [1920x1080px]");
-define("LAN_LZ_THEMEPREF_03", "Cesta URL k hlavičce videa ve formátu mp4");
-define("LAN_LZ_THEMEPREF_04", "Registrace/přihlášení umístění");
-define("LAN_LZ_THEMEPREF_05", "nahoru");
-define("LAN_LZ_THEMEPREF_06", "dolní");
+define("LAN_LZ_THEMEPREF_03", "URL cesta k záhlaví video ve formátu MP4");
+define("LAN_LZ_THEMEPREF_04", "Umístění Registrace / Přihlášení");
+define("LAN_LZ_THEMEPREF_05", "nahoře");
+define("LAN_LZ_THEMEPREF_06", "dole");
