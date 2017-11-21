b23b6c2c7bcbfc65e821ed3ebfda9f2ac2ee73db
Roland Graf <oxigen.rg@gmail.com>
2017-11-21 23:36:14 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/comment_menu/languages/Czech.php
+++ e107_plugins/comment_menu/languages/Czech.php
@@ -4,14 +4,17 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:10:45
+|        Last Modified: 2017/11/21 23:15:13
 |
-|        $Author: Oxigen $
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/21 23:15:13 $
+|        $Author: OxigenO2 $
 +---------------------------------------------------------------+
 */
+
 define("CM_L1", "Zatím žádné komentáře.");
-define("CM_L2", "");
-define("CM_L3", "Záhlaví");
+define("CM_L3", "Hlavička");
 define("CM_L4", "Kolik zobrazit komentářů?");
 define("CM_L5", "Kolik zobrazit znaků?");
 define("CM_L6", "Přípona pro příliš dlouhé komentáře?");
@@ -20,5 +23,3 @@ define("CM_L8", "Konfigurace menu nových komentářů");
 define("CM_L11", "v");
 define("CM_L12", "Re:");
 define("CM_L13", "Napsal");
-
-
