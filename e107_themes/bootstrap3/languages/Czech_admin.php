a9e3a3555a06e323baa1fc593cee9ea50ad54426
Roland Graf <oxigen.rg@gmail.com>
2017-11-21 22:52:46 +0100

    Updated to 2.1.7-alpha

--- e107_themes/bootstrap3/languages/Czech_admin.php
+++ e107_themes/bootstrap3/languages/Czech_admin.php
@@ -1,17 +1,29 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/25 16:07:05
+|
+|        $URL: www.e107.funsite.cz $
+|        $Revision: 2.16 $
+|        $Id: 2017/10/25 16:07:05 $
+|        $Author: Oxigen O2 $
++---------------------------------------------------------------+
+*/
 
 define("LAN_THEMEPREF_00", "Značky:");
-define("LAN_THEMEPREF_01", "Navigační panel Zarovnání:");
-define("LAN_THEMEPREF_02", "Registrace/přihlášení umístění:");
-define("LAN_THEMEPREF_03", "Bootswatch styly:");
+define("LAN_THEMEPREF_01", "Šířka hlavního menu:");
+define("LAN_THEMEPREF_02", "Registrace/Přihlášení umístění");
+define("LAN_THEMEPREF_03", "Bootswatch styl:");
 define("LAN_THEMEPREF_04", "Název webu");
 define("LAN_THEMEPREF_05", "Logo");
-define("LAN_THEMEPREF_06", "Logo a název webu");
+define("LAN_THEMEPREF_06", "Logo &amp; Název webu");
 define("LAN_THEMEPREF_07", "vlevo");
-define("LAN_THEMEPREF_08", "Doprava");
-define("LAN_THEMEPREF_09", "nahoru");
-define("LAN_THEMEPREF_10", "dolní");
+define("LAN_THEMEPREF_08", "vpravo");
+define("LAN_THEMEPREF_09", "nahoře");
+define("LAN_THEMEPREF_10", "dole");
+
+
+?>
