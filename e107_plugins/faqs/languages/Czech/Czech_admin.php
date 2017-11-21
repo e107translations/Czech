2632d0855dabc31b26f80fa57cbbbe840e37fe83
Roland Graf <oxigen.rg@gmail.com>
2017-11-21 23:37:59 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/faqs/languages/Czech/Czech_admin.php
+++ e107_plugins/faqs/languages/Czech/Czech_admin.php
@@ -1,18 +1,52 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
+/*
+* Copyright (c) e107 Inc 2008-2013 - e107.org, 
+* Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+*
+* "FAQ plugin" admin-area language definitions
+*/
 define("LANA_FAQ_QUESTION", "Otázka");
-define("LANA_FAQ_ANSWER", "Odpověď");
-define("LANA_FAQ_COMMENT", "Komentář");
-define("LANA_FAQ_UNAME", "Uživatelské jméno");
-define("LANA_FAQ_ULOGINNAME", "Přihlášení uživatele");
-define("LANA_FAQ_TAGS", "Značky");
-define("LANA_FAQ_TAGS_HELP", "Čárkami oddělený seznam značek");
-define("LANA_FAQ_METAD", "Meta popis");
-define("LANA_FAQ_METAK", "META klíčová slova");
-define("LANA_FAQ_PREF_1", "Povolit odesílání z FAQ:");
-define("LANA_FAQ_PREF_2", "'Položit otázku' je omezeno na");
-define("LANA_FAQ_PREF_3", "Použití klasické rozložení");
+define("LANA_FAQ_QUESTIONS", "Otázky");
+define ("LANA_FAQ_ANSWER", "Odpověď");
+define ("LANA_FAQ_UNANSWERED", "Nezodpovězeno");
+define ("LANA_FAQ_COMMENT", "Třída komentářů");
+define ("LANA_FAQ_ULOGINNAME", "Přihlášení uživatele");
+define("LANA_FAQ_TAGS", "Tagy");
+define ("LANA_FAQ_TAGS_HELP", "Seznam tagů oddělených čárkami");
+define ("LANA_FAQ_METAD", "Meta Popis");
+define ("LANA_FAQ_METAK", "Meta klíčová slova");
+
+define ("LANA_FAQ_PREF_1", "Povolit vkládání otázek pro:");
+define ("LANA_FAQ_PREF_2", "Omezení otázek pro:");
+define ("LANA_FAQ_PREF_3", "Použít klasické rozvržení");
+define ("LANA_FAQ_PREF_4", "Limit dotazů na jednoho uživatele");
+define ("LANA_FAQ_PREF_5", "Limit počtu znaků pro dotaz");
+define ("LANA_FAQ_PREF_6", "Dotazy omezené na");
+define ("LANA_FAQ_PREF_7", "Potvrzení zadaných otázek");
+define ("LANA_FAQ_PREF_8", "Typ seznamu");
+define ("LANA_FAQ_PREF_9", "Neřízený seznam");
+define ("LANA_FAQ_PREF_10", "Řízený seznam");
+define ("LANA_FAQ_PREF_11", "Název stránky");
+define ("LANA_FAQ_PREF_12", "Otázky jsou 'nové' pouze");
+define ("LANA_FAQ_PREF_13", "Zobrazovat celkový FAQ");
+define ("LANA_FAQ_PREF_14", "Zobrazit časové razítko");
+define ("LANA_FAQ_PREF_15", "Zobrazit sociální tlačítka");
+define ("LANA_FAQ_PREF_16", "Specifické řazení");
+define ("LANA_FAQ_PREF_17", "Všechny jazyky");
+define ("LANA_FAQ_PREF_18", "Vzestup podle ID");
+define ("LANA_FAQ_PREF_19", "Sestupně podle ID");
+define ("LANA_FAQ_PREF_20", "Vzestupně podle data");
+define ("LANA_FAQ_PREF_21", "Sestupně podle data");
+define ("LANA_FAQ_PREF_22", "Správa dotazů");
+define ("LANA_FAQ_PREF_23", "Správa kategorií");
+
+define("LANA_FAQ_PREF_HELP_1", "0 = žádný limit");
+define("LANA_FAQ_PREF_HELP_2", "Chcete-li použít výchozí, ponechte prázdné");
+
+
+define("LANA_FAQ_CRON_1", "Nezodpovězené otázky");
+define("LANA_FAQ_CRON_2", "Posílá zprávu o nezodpovězených otázkách na adresu [x].");
+define("LANA_FAQ_CRON_3", "[x] nezodpovězené dotazy v [y]");
+define("LANA_FAQ_CRON_4", "Přihlaste se na své stránky, kde můžete tyto otázky přečíst a odpovědět pomocí níže uvedeného odkazu.");
+define("LANA_FAQ_CRON_5", "Poslední [x] otázky jsou zobrazeny níže.");
+define("LANA_FAQ_CRON_6", "[x] nezodpovězená otázka od [y].");
--- e107_plugins/faqs/languages/Czech/Czech_front.php
+++ e107_plugins/faqs/languages/Czech/Czech_front.php
@@ -1,19 +1,28 @@
 <?php
 /*
-+---------------------------------------------------------------+
-|        e107 website content management system Czech Language File
-|        Released under the terms and conditions of the
-|        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:15:35
-|
-|        $Author: Oxigen $
-+---------------------------------------------------------------+
+ * e107 website system
+ *
+ * Copyright (C) 2008-2016 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * FAQ Plugin Global Language Definitions
+ *
 */
 
-define("LAN_FAQS_TAGS", "Značky");
-define("LAN_FAQS_FILTER_ACTIVE", "Filtr aktivní");
+define("LAN_FAQS_TAGS", "Tagy");
+define("LAN_FAQS_FILTER_ACTIVE", "Filtr");
 define("LAN_FAQS_NONE_AVAILABLE", "V současné době nejsou k dispozici žádné dotazy.");
-define("LAN_FAQS_ASKQUESTION_AFTER", "Děkuju. Vaše otázka byla uložena a budou zodpovězeny co nejdříve.");
-
-
-?>
+define("LAN_FAQS_ASKQUESTION_AFTER", "Děkuji, Vaše otázka byla uložena a doufejme, že bude brzy zodpovězena.");
+define("LAN_FAQS_LIMIT_REACHED", "Dosáhli jste maximálního počtu nových otázek. Můžete položit další otázku, jakmile budou vaše stávající otázky zodpovězeny.");
+define("LAN_FAQS_REMOVE", "Odstranit");
+define("LAN_FAQS_RESET", "Resetovat");
+define("LAN_FAQS_X_NOT_FOUND", "[x]: Nebylo nalezeno.");
+define("LAN_FAQS_MODERATE_COMMENTS", "moderovat komentáře");
+define("LAN_FAQS_BACK_TO_CATEGORIE", "Zpět na kategorie");
+define("LAN_FAQS_FAQ", "FAQ");
+define("LAN_FAQS_ENTER_A_QUESTION_HERE", "Zadejte svou otázku zde...");
+define("LAN_FAQS_CHAR_LIMIT", "maximální počet znaků");
+define("LAN_FAQS_PLEASE_REGISTER", "Pro vložení otazky se prosím [register] nebo se přihlaste.");
+define("LAN_FAQS_NOT_PERMITTE", "Nepovoleno.");
+define("LAN_FAQS_YOUR_REQUESTED_FAQS", "Byli jste požádáni o odpověď na následující otázky.");
