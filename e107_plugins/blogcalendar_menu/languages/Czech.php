fb2a498d822a12c4ed888140be51716d2e1dfdf1
Roland Graf <oxigen.rg@gmail.com>
2017-11-21 23:32:58 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/blogcalendar_menu/languages/Czech.php
+++ e107_plugins/blogcalendar_menu/languages/Czech.php
@@ -4,18 +4,19 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:09:43
+|        Last Modified: 2017/11/21 23:11:27
 |
-|        $Author: Oxigen $
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/21 23:11:27 $
+|        $Author: OxigenO2 $
 +---------------------------------------------------------------+
 */
 
-define("BLOGCAL_L1", "Novinky za měsíc");
-define("BLOGCAL_L2", "archiv");
-define("BLOGCAL_1", "novinek");
-define("BLOGCAL_CONF1", "Měsíce/řádek");
-define("BLOGCAL_CONF2", "Odsazení obsahu buněk");
-define("BLOGCAL_CONF4", "BlogCal Menu konfigurace");
-define("BLOGCAL_ARCHIV1", "Vyberte archiv");
-
-
+define("BLOGCAL_L1", "Novinky podle měsíce");
+define("BLOGCAL_L2", "Archiv");
+define("BLOGCAL_1", "Novinky");
+define("BLOGCAL_CONF1", "Měsíce/řada");
+define("BLOGCAL_CONF2", "Vnitřní okraj");
+define("BLOGCAL_CONF4", "Nastavení BlogCal Menu");
+define("BLOGCAL_ARCHIV1", "Vybrat archiv");
