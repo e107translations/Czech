386036c493464916812df5407930680246916afc
Roland Graf <oxigen.rg@gmail.com>
2017-11-22 01:55:14 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/social/languages/Czech/Czech_front.php
+++ e107_plugins/social/languages/Czech/Czech_front.php
@@ -1,20 +1,29 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/11/22 01:41:34
+|
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/22 01:41:34 $
+|        $Author: OxigenO2 $
++---------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_SOCIAL_000", "Podíl na [x]");
-define("LAN_SOCIAL_001", "Jako na [x]");
-define("LAN_SOCIAL_002", "E-mailem někomu");
-define("LAN_SOCIAL_003", "+ 1 na Google");
-define("LAN_SOCIAL_004", "Přidat [x]");
+define("LAN_SOCIAL_000", "Sdílet na [x]");
+define("LAN_SOCIAL_001", "Líbí se [x]");
+define("LAN_SOCIAL_002", "Odeslat e-mail");
+define("LAN_SOCIAL_003", "+1 na Google");
+define("LAN_SOCIAL_004", "Přidat do [x]");
 define("LAN_SOCIAL_005", "Podívejte se na tento odkaz:");
-define("LAN_SOCIAL_100", "Nelze zobrazit informační kanál. Facebook App ID není definován v předvolbách.");
-define("LAN_SOCIAL_200", "Nelze zobrazit informační kanál. Twitter URL nebyl definován v předvolbách.");
-define("LAN_SOCIAL_201", "Podle fanoušků");
-define("LAN_SOCIAL_202", "Publikování na Twitteru");
-define("LAN_SOCIAL_203", "Zadejte vaše pípání.");
-define("LAN_SOCIAL_204", "Podíl");
-define("LAN_SOCIAL_205", "Nelze vykreslit komentáře. Chybějící ID aplikace Facebook.");
-define("LAN_SOCIAL_WARNING", "Facebook Komentáře vyžaduje, že máte facebook ID aplikace. Viz oblast 'sociální přihlášení' v předvolbách správce přidat.");
+define("LAN_SOCIAL_100", "Zprávy nelze zobrazit! ID aplikace Facebook není správně definováno!");
+define("LAN_SOCIAL_200", "Zprávy nelze zobrazit! Adresa URL služby Twitter není správně zadána!");
+define("LAN_SOCIAL_201", "Tweet příspěvky");
+define("LAN_SOCIAL_202", "Odeslat na Twitter");
+define("LAN_SOCIAL_203", "Zadejte svůj tweet zde.");
+define("LAN_SOCIAL_204", "Sdílet");
+define("LAN_SOCIAL_205", "Nelze vypsat komentáře. Chybí ID aplikace Facebook.");
+define("LAN_SOCIAL_WARNING", "Příspěvky na Facebooku vyžadují ID aplikace. Přejděte na 'Registraci / Přihlášení' v administračním rozhraní do nastavení.");
--- e107_plugins/social/languages/Czech/Czech_global.php
+++ e107_plugins/social/languages/Czech/Czech_global.php
@@ -1,11 +1,20 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/11/22 01:41:10
+|
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/22 01:41:10 $
+|        $Author: OxigenO2 $
++---------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("LAN_PLUGIN_SOCIAL_DESCR", "Přidá možnosti nahradit modul komentáře e107 s Facebook. Přidáte Twitter kanály na vaše stránky. atd.");
-define("LAN_PLUGIN_SOCIAL_SIGNIN", "Přihlaste se:");
-define("LAN_PLUGIN_SOCIAL_XUP_SIGNUP", "Přihlásit se s účtem [x]");
-define("LAN_PLUGIN_SOCIAL_XUP_REG", "Registrace účtu [x]");
-define("LAN_PLUGIN_SOCIAL_NAME", "Sociální");
+define("LAN_PLUGIN_SOCIAL_DESCR", "Přidá možnost, jak nahradit modul pro komentáře e107 pomocí Facebooku. Přidat kanály Twitter na vaše stránky. atd.");
+define("LAN_PLUGIN_SOCIAL_SIGNIN", "Přihlásit se pomoci:");
+define("LAN_PLUGIN_SOCIAL_XUP_SIGNUP", "Přihlaste se pomocí účtu [x]");
+define("LAN_PLUGIN_SOCIAL_XUP_REG", "Registrovat se svým účtem [x]");
+define("LAN_PLUGIN_SOCIAL_NAME", "Sociální služby");
