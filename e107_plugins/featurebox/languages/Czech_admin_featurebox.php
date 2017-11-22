f19f04caf871690e41d31ec453012a6d2ad39b88
Roland Graf <oxigen.rg@gmail.com>
2017-11-22 01:32:44 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/featurebox/languages/Czech_admin_featurebox.php
+++ e107_plugins/featurebox/languages/Czech_admin_featurebox.php
@@ -4,9 +4,12 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:15:39
+|        Last Modified: 2017/11/21 23:22:39
 |
-|        $Author: Oxigen $
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/21 23:22:39 $
+|        $Author: OxigenO2 $
 +---------------------------------------------------------------+
 */
 
@@ -17,14 +20,12 @@ define("FBLAN_14", "Zobrazit pouze tento text");
 define("FBLAN_22", "Nastavení vzhledu");
 define("FBLAN_23", "Podle šablony stránky");
 define("FBLAN_24", "Jednoduchý text");
-
-
-define("FBLAN_25", "Neexistují žádné featurebox v položkách přiřazených k šabloně [x].");
-define("FBLAN_26", "Obrázek a Video");
-define("FBLAN_27", "Odkaz na obrázek");
-define("FBLAN_28", "Featurebox Menu kategorie");
-define("FBLAN_29", "Kategorie pro featurebox menu");
-define("FBLAN_30", "Kategorie šablon");
-define("FBLAN_31", "Náhodné");
+define("FBLAN_25", "K šabloně [x] nejsou přiřazeny žádné položky seznamu.");
+define("FBLAN_26", "Obrázek / Video");
+define("FBLAN_27", "Obrázek jako odkaz");
+define("FBLAN_28", "Menu Featurebox - kategorie");
+define("FBLAN_29", "Kategorie, kterou chcete zobrazit v menu featurebox");
+define("FBLAN_30", "Šablony kategorie");
+define("FBLAN_31", "Náhodný");
 define("FBLAN_32", "Parametry (volitelné)");
-define("FBLAN_33", "Volitelné parametry Javascript (formát může změnit)");
+define("FBLAN_33", "Volitelné parametry jazyka Javascript (formát se může změnit)");
--- e107_plugins/featurebox/languages/Czech_global.php
+++ e107_plugins/featurebox/languages/Czech_global.php
@@ -4,19 +4,20 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:16:09
+|        Last Modified: 2017/11/21 23:22:55
 |
-|        $Author: Oxigen $
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/21 23:22:55 $
+|        $Author: OxigenO2 $
 +---------------------------------------------------------------+
 */
 
-define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
-define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Displays an animated area on the top of your page with news-items and other content you would like to feature.");
-define("LAN_PLUGIN_FEATUREBOX_BATCH", "Vytvořit položku Featurebox");
-define("FBLAN_INSTALL_01", "Adding Default category table data.");
-define("FBLAN_INSTALL_02", "Adding Default table data.");
+define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature box");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Tento doplněk vám umožňuje zobrazit blok s textem nad vašimi novinkami. Texty se mohou také měnit, buď náhodně nebo podle daného pořadí.");
+define("LAN_PLUGIN_FEATUREBOX_BATCH", "Přidat položku");
+define("FBLAN_INSTALL_01", "Přidání výchozích kategorií.");
+define("FBLAN_INSTALL_02", "Přidání výchozích hodnot.");
 define("FBLAN_INSTALL_03", "Nepřiřazené");
 define("FBLAN_INSTALL_04", "Carousel");
 define("FBLAN_INSTALL_05", "Tabulátory");
-
-
