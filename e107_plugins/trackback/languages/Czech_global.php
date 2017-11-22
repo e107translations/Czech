d6cf5b41ef70a3ee414a0033581b72823dd993d7
Roland Graf <oxigen.rg@gmail.com>
2017-11-22 01:51:52 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/trackback/languages/Czech_global.php
+++ e107_plugins/trackback/languages/Czech_global.php
@@ -4,13 +4,14 @@
 |        e107 website content management system Czech Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2015/12/20 20:27:16
+|        Last Modified: 2017/11/22 01:38:30
 |
-|        $Author: Oxigen $
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/22 01:38:30 $
+|        $Author: OxigenO2 $
 +---------------------------------------------------------------+
 */
 
-define("LAN_PLUGIN_TRACKBACK_NAME", "Trackback");
-define("LAN_PLUGIN_TRACKBACK_DESCRIPTION", "Tento doplněk vám umožní používat funkci zpětných odkazů pro vaše novinky.");
-
-
+define("LAN_PLUGIN_TRACKBACK_NAME", "Zpětný odkaz");
+define("LAN_PLUGIN_TRACKBACK_DESCRIPTION", "Tento plugin vám umožní použít trackback v novinky příspěvky.");
