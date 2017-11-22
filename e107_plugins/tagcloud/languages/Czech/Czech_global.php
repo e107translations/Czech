b4b2cabe2adaa5ee63c0c4ea4db92bbae44f80d2
Roland Graf <oxigen.rg@gmail.com>
2017-11-22 01:53:56 +0100

    Updated to 2.1.7-alpha

--- e107_plugins/tagcloud/languages/Czech/Czech_global.php
+++ e107_plugins/tagcloud/languages/Czech/Czech_global.php
@@ -1,8 +1,17 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/11/22 01:40:43
+|
+|        $URL: https://github.com/oxigeno2/e107 $
+|        $Revision: 2.1.7 $
+|        $Id: 2017/11/22 01:40:43 $
+|        $Author: OxigenO2 $
++---------------------------------------------------------------+
+*/
 
 define("LAN_PLUGIN_TAGCLOUD_NAME", "Tag Cloud");
-define("LAN_PLUGIN_TAGCLOUD_DESCRIPTION", "Jednoduchý tagcloud menu pro vaše webové stránky e107.");
+define("LAN_PLUGIN_TAGCLOUD_DESCRIPTION", "Jednoduché menu tagcloud pro vaše webové stránky e107.");
