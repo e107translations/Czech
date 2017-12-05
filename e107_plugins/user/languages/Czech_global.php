32263f98bc0d59818bc13a3a9e886337a6f52612
oxigeno2 <oxigen.rg@gmail.com>
2017-12-05 17:35:56 +0100

    Updated to 2.1.7-alpha

---
+++ e107_plugins/user/languages/Czech_global.php
@@ -0,0 +1,17 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Czech Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/10/08 21:55:39
+|
+|        $Author: OxigenO2 $
++---------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_USER_NAME", "Uživatelské menu");
+define("LAN_PLUGIN_USER_DESC", "Uživatelské menu pro volbu jazyka a vzhledu");
+
+
+?>
