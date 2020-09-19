ff8082de8e28eace15ef7d7647858c37b4a502c1
OxigenO2 <oxigen.rg@gmail.com>
2020-09-19 17:50:07 +0200

    Delete theme.php

--- e107_languages/Czech/admin/help/theme.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-if (!defined('e107_INIT')) { exit; }
-
-$text = "Správce vzhledu vám umožňuje nastavení šablony pro  veřejnou a administrativní část vašich stránek.";
-$ns -> tablerender("Nápověda - Správce vzhledu", $text);
-?>
